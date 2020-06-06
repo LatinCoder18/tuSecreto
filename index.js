b826c4fb89acbc8eb2924707243898cdde5aa6d6
Adonys <avaldesa007@gmail.com>
2020-06-06 11:33:34 -0400

    Telegram bot Feactures Add

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var request = require('request');
 var irc = require('irc');
+const TelegramBot = require('node-telegram-bot-api');
 var CronJob = require('cron').CronJob;
 var fs = require('fs');
 
 var config = require('./config.json');
 //var secrets = require('./database.json');
+const token = '629645239:AAEjxhsWgTz4W22BECAmI8dhtl64WNre7z8';
 
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
 var client = new irc.Client(config.bot.server, config.bot.name, {
     userName: config.bot.username,
@@ -56,6 +60,20 @@ var jobAPP = new CronJob(
 );
 jobAPP.start();
 
+var job = new CronJob(
+    '0 */45 * * * *',
+    function () {
+        console.log('Hola');
+
+    },
+    null,
+    false,
+    'America/Havana'
+);
+job.start();
+
+    
+ 
 function decirSecreto() {
     fs.readFile('secreto.txt', 'utf8', function (err, data) {
         if (err) {
@@ -65,6 +83,9 @@ function decirSecreto() {
         data = data.split('\n');
         console.log(data);
         var aleatorio = Math.round(Math.random() * data.length);
+        
         client.say('#secretos',data[aleatorio]);
+        bot.sendPhoto('-1001215282688',"secreto.png",{caption : data[aleatorio]} );
+
     });
 }
